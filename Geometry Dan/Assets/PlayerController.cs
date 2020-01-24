1345a3fe4b152f6cc7a484b006c1478157b9222d
codpro2005 <40277095+codpro2005@users.noreply.github.com>
2020-01-24 16:27:08 +0100

    Added on scream and additional stuff lol

--- Geometry Dan/Assets/PlayerController.cs
+++ Geometry Dan/Assets/PlayerController.cs
@@ -1,21 +1,61 @@
-﻿using System.Linq;
+﻿using MyBox;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public enum State
+{
+    Run,
+    Fly
+}
+
+//public class Active
+//{
+//    public bool isActive;
+//}
+
+//public class Active<T>
+//{
+//    public bool isActive;
+//    public T Value;
+//}
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Vector2 xForce;
+    [SerializeField] private bool enableActionOnTouch;
+    [SerializeField] private bool enableActionOnScream;
+    [SerializeField] [ConditionalField("enableJumpOnScream")] private float jumpThreshold;
     [SerializeField] private GameObject jumpResetOnCollideWith;
+    [SerializeField] private GameObject killOnCollideWith;
+    [SerializeField] private GameObject triggerFlyOnCollideWith;
 
     private Rigidbody2D currentRigidbody2D;
+
+    private State state;
     private GameObject jumpResetOnCollideWithReference;
-    private bool triggerForce;
-    private bool jumpReset;
+    private GameObject killOnCollideWithReference;
+    private GameObject triggerFlyOnCollideWithReference;
+    private AudioClip microphoneInput;
+    private bool microphoneExists;
+    private bool touchPhaseBegan;
+    private float volume;
+    private bool touchesGround;
+    private Handler handler;
 
     // Awake is called before Start and should be used as the constructor
     private void Awake()
     {
         this.currentRigidbody2D = this.GetComponent<Rigidbody2D>();
         this.jumpResetOnCollideWithReference = GameObject.Find(this.jumpResetOnCollideWith.name);
+        this.killOnCollideWithReference = GameObject.Find(this.killOnCollideWith.name);
+        this.triggerFlyOnCollideWithReference = GameObject.Find(this.triggerFlyOnCollideWith.name);
+        this.handler = GameObject.Find("Handler").GetComponent<Handler>();
+        if (Microphone.devices.Length > 0)
+        {
+            this.microphoneExists = true;
+            this.microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
+        }
     }
 
     // Start is called before the first frame update
@@ -27,19 +67,45 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-	    if (Input.touches.Any(touch => touch.phase == TouchPhase.Began))
+        var currentMicPosition = Microphone.GetPosition(null);
+        if (microphoneExists && currentMicPosition > 0)
+        {
+            int dec = 128;
+            float[] waveData = new float[dec];
+            int micPosition = currentMicPosition - (dec + 1);
+            this.microphoneInput.GetData(waveData, micPosition);
+            this.volume = Mathf.Sqrt(Mathf.Sqrt(waveData.Select(single => Mathf.Pow(single, 2)).Max()));
+        }
+
+        if (Input.touches.Any(touch => touch.phase == TouchPhase.Began))
 	    {
-		    this.triggerForce = true;
+		    this.touchPhaseBegan = true;
 	    }
 
     }
 
     private void FixedUpdate()
     {
-        if (this.triggerForce && this.jumpReset)
+        switch (this.state)
         {
-            this.currentRigidbody2D.AddForce(this.xForce);
-            this.triggerForce = false;
+            case State.Run:
+                if (this.touchesGround && ((this.enableActionOnTouch && this.touchPhaseBegan) || (this.enableActionOnScream && this.volume > this.jumpThreshold)))
+                {
+                    this.currentRigidbody2D.AddForce(this.xForce);
+                    this.touchPhaseBegan = false;
+                    this.volume = 0;
+                }
+                break;
+            case State.Fly:
+                if (this.enableActionOnScream && this.volume > this.jumpThreshold)
+                {
+                    Debug.Log(this.xForce * this.volume - new Vector2(0, 0.5f));
+                    this.currentRigidbody2D.AddForce(this.xForce * this.volume - new Vector2(0, 0.5f));
+                    this.volume = 0;
+                }
+                break;
+            default:
+                break;
         }
     }
 
@@ -48,8 +114,16 @@ public class PlayerController : MonoBehaviour
 	    var collision2DTransfromTag = collision2D.transform.tag;
 	    if (collision2DTransfromTag == this.jumpResetOnCollideWithReference.tag)
 	    {
-		    this.jumpReset = true;
-	    }
+		    this.touchesGround = true;
+	    } else
+        if (collision2DTransfromTag == this.killOnCollideWithReference.tag)
+        {
+            this.handler.ReloadScene();
+        } else
+        if (collision2DTransfromTag == this.triggerFlyOnCollideWithReference.tag)
+        {
+            this.state = this.state == State.Fly ? State.Run : State.Fly;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision2D)
@@ -57,7 +131,7 @@ public class PlayerController : MonoBehaviour
 	    var collision2DTransfromTag = collision2D.transform.tag;
 	    if (collision2DTransfromTag == this.jumpResetOnCollideWithReference.tag)
 	    {
-		    this.jumpReset = false;
+		    this.touchesGround = false;
 	    }
     }
 }
