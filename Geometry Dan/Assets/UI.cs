a805d9635db65cfc30409dd12355360b052410d7
codpro2005 <40277095+codpro2005@users.noreply.github.com>
2020-01-30 01:43:25 +0000

    Adjusted UI Script to always autosize & place any visuals responsively.

--- Geometry Dan/Assets/UI.cs
+++ Geometry Dan/Assets/UI.cs
@@ -22,27 +22,30 @@ public class UI : MonoBehaviour
 	[SerializeField] private ProgressionBar progressionBar;
 
 	private Transform currentTransform;
-
+	private Camera currentCamera;
 	private GameObject valueToModifyReference;
 	private Transform xDeterminedByReferenceTransform;
 	private float startX;
 	private float endX;
+	private float cameraStartSize;
 	private GameObject inactiveProgression;
+	private Vector2 startProgressionScale;
 	private GameObject activeProgression;
 	private float progression;
-
+	private float cameraSizeMultiplier => this.currentCamera.orthographicSize / this.cameraStartSize;
 	private static string Name => "UI";
 
 	// Awake is called before Start and should be used as the constructor
 	private void Awake()
 	{
 		this.currentTransform = this.GetComponent<Transform>();
-
+		this.currentCamera = this.GetComponent<Camera>();
 		this.valueToModifyReference = GameObject.Find(this.progressionBar.valueToModify.transform.name);
 		this.xDeterminedByReferenceTransform = GameObject.Find(this.progressionBar.xDeterminedBy.transform.name).transform;
 		this.startX = this.xDeterminedByReferenceTransform.position.x;
 		this.endX = GameObject.Find(this.progressionBar.endXDeterminedby.transform.name).GetComponent<SceneHandler>()
 			.GetFinishLineX();
+		this.cameraStartSize = this.currentCamera.orthographicSize;
 	}
 
 	// Start is called before the first frame update
@@ -53,6 +56,7 @@ public class UI : MonoBehaviour
 		var identity = Quaternion.identity;
 
 		UI.CreateProgressionGameObject(out this.inactiveProgression, progressionBarObject, progressionBarPosition, identity, this.progressionBar.inactive, 0);
+		this.startProgressionScale = this.inactiveProgression.transform.localScale;
 		UI.CreateProgressionGameObject(out this.activeProgression, progressionBarObject, progressionBarPosition, identity, this.progressionBar.active, inactiveProgression.GetComponent<SpriteRenderer>().sortingOrder + 1);
 	}
 
@@ -64,13 +68,14 @@ public class UI : MonoBehaviour
 		this.progression = totalProgression < 0 ? 0 : totalProgression < 1 ? totalProgression : 1;
 
 		var inactiveProgressionTransform = this.inactiveProgression.transform;
-		inactiveProgressionTransform.position = (Vector2)this.currentTransform.position + this.progressionBar.offset;
+		inactiveProgressionTransform.position = (Vector2)this.currentTransform.position + this.progressionBar.offset * this.cameraSizeMultiplier;
+		inactiveProgressionTransform.localScale = this.startProgressionScale * this.cameraSizeMultiplier;
 
 		var inactiveProgressionBarPosition = inactiveProgressionTransform.position;
 		var inactiveProgressionBarLocalScaleX = inactiveProgressionTransform.localScale.x;
 		var activeProgressionTransform = this.activeProgression.transform;
 		activeProgressionTransform.position = new Vector2(inactiveProgressionBarPosition.x - (inactiveProgressionBarLocalScaleX - activeProgressionTransform.localScale.x) / 2, inactiveProgressionBarPosition.y);
-		activeProgressionTransform.localScale = new Vector2(inactiveProgressionBarLocalScaleX * this.progression, activeProgressionTransform.localScale.y);
+		activeProgressionTransform.localScale = new Vector2(inactiveProgressionBarLocalScaleX * this.progression, inactiveProgressionTransform.localScale.y);
 
 		var valueToModifyTmpUgui = this.valueToModifyReference.GetComponent<TextMeshProUGUI>();
 		if (valueToModifyTmpUgui.enabled)
